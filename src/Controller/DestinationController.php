1189fd6a113bdafa50e63a19af29259e43a90ded
ali <alikhyatii@gmail.com>
2020-05-27 23:07:35 +0000

    DestinationDetails

--- src/Controller/DestinationController.php
+++ src/Controller/DestinationController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VilleRepository;
+
 
 class DestinationController extends AbstractController
 {
     /**
      * @Route("/destination", name="destination")
      */
-    public function index()
+    public function index(VilleRepository $villeRepository)
     {
+        $villes = $villeRepository->findAll();
+        // $region = $regionRepository->findOneBy(array('id' => ));
         return $this->render('destination/index.html.twig', [
-            'controller_name' => 'DestinationController',
+            'controller_name' => 'DestinationController', 'villes' => $villes,
         ]);
     }
 }
--- src/Controller/DestinationDetailsController.php
+++ src/Controller/DestinationDetailsController.php
@@ -21,7 +21,7 @@ class DestinationDetailsController extends AbstractController
     public function index(Request $request, $name, MarkerRepository $markerRepository, HotelRepository $hotelRepository, RestoRepository $restoRepository, 
     ActiviteRepository $activiteRepository, CampingRepository $campingRepository, VilleRepository $villeRepository)
     {
-        $ville = $villeRepository->findBy(array('name' => $name));
+        $ville = $villeRepository->findOneBy(array('name' => $name));
         $hotelMarkers = $markerRepository->findBy(array('type' => 'hotel', 'ville' => $ville));
         $restoMarkers = $markerRepository->findBy(array('type' => 'resto', 'ville' => $ville));
         $activiteMarkers = $markerRepository->findBy(array('type' => 'activite', 'ville' => $ville));
@@ -32,7 +32,7 @@ class DestinationDetailsController extends AbstractController
         $campings = $campingRepository->findBy(array('ville' => $ville));
         $villeMarker = $markerRepository->findOneBy(array('type' => 'ville', 'ville' => $ville));
         return $this->render('destination_details/index.html.twig', [
-            'controller_name' => 'DestinationDetailsController', 'villeMarker' => $villeMarker,
+            'controller_name' => 'DestinationDetailsController', 'villeMarker' => $villeMarker, 'ville' => $ville,
              'hotelMarkers' => $hotelMarkers, 'hotels' => $hotels, 'restos' => $restos, 'activites' => $activites, 'campings' => $campings,
              'restoMarkers' => $restoMarkers, 'activiteMarkers' => $activiteMarkers, 'campingMarkers' => $campingMarkers,
         ]);
