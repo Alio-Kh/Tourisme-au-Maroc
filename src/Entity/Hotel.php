ccda958869520e9ab04ddb786c41dc539e08c3e1
ali <alikhyatii@gmail.com>
2020-05-27 09:12:43 +0000

    init

---
+++ src/Entity/Hotel.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HotelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=HotelRepository::class)
+ */
+class Hotel
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $imgPath;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Marker::class)
+     */
+    private $marker;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getImgPath(): ?string
+    {
+        return $this->imgPath;
+    }
+
+    public function setImgPath(string $imgPath): self
+    {
+        $this->imgPath = $imgPath;
+
+        return $this;
+    }
+
+    public function getMarker(): ?Marker
+    {
+        return $this->marker;
+    }
+
+    public function setMarker(?Marker $marker): self
+    {
+        $this->marker = $marker;
+
+        return $this;
+    }
+
+//     /**
+//     * toString
+//     * @return string
+//     */
+//    public function __toString()
+//    {
+//            return $this->getMarker();
+//    }
+}
